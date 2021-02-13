d8926d71cbb222b6bbd68d537ac38653bffaeb0a
BlackFox <bigdeli.ali3@gmail.com>
2021-02-13 21:57:10 +0330

    changes to time picker

--- booking_app/forms.py
+++ booking_app/forms.py
@@ -2,8 +2,17 @@ from .models import Patient
 from django import forms
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
 class PatientForm (forms.ModelForm):
     class Meta : 
         model = Patient
         fields = '__all__'
+        widgets = {
+            'appointment_date': DateInput(),
+            #'appointment_time': TimeInput(),
+        }
     
